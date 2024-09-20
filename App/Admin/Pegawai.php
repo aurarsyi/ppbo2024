84de79b95460fc4d20e49a2996694d3c0e26f4d7
aurarsyi <auraarsyi88@gmail.com>
2024-09-20 16:28:54 +0700

    Praktikum 6E - Menggunakan Pewarisan

---
+++ App/Admin/Pegawai.php
@@ -0,0 +1,62 @@
+<?php
+
+
+namespace App\Admin;
+
+class Pegawai {
+    // Atribut
+    public $nip;
+    public $nama;
+    protected $no_hp;
+    public $alamat;
+
+    
+    public function __construct($nip, $nama, $no_hp, $alamat) {
+        $this->nip = $nip;
+        $this->nama = $nama;
+        $this->no_hp = $no_hp;
+        $this->alamat = $alamat;
+    }
+
+    
+    public function cekIn() {
+        
+        return true;
+    }
+
+    
+    public function cekOut() {
+        
+        return true;
+    }
+
+    
+    public function getNoHp() {
+        return $this->no_hp;
+    }
+
+    
+    public function setNoHp($no_hp) {
+        $this->no_hp = $no_hp;
+    }
+}
+
+
+class TenagaKependidikan extends Pegawai {
+    
+    public $gaji_pokok;
+
+    
+    public function __construct($nip, $nama, $no_hp, $alamat, $gaji_pokok) {
+        parent::__construct($nip, $nama, $no_hp, $alamat);
+        $this->gaji_pokok = $gaji_pokok;
+    }
+
+    // Method cuti
+    public function cuti() {
+        // Implementasi logika cuti
+        echo "{$this->nama} sedang cuti.";
+    }
+}
+
+
